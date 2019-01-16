e90468454edd22e530170cc863779e9ce1dab9b4
GeovanniOj <geovannirock@gmail.com>
2019-01-16 18:07:29 -0200

    View Screen structure has been created

--- organDisplay/organDisplay/ViewController.swift
+++ organDisplay/organDisplay/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import SceneKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var scene3D: SCNView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-
+  @IBAction func antAction(_ sender: Any) {
+  }
+  
+  @IBAction func postAction(_ sender: Any) {
+  }
+  @IBAction func latAction(_ sender: Any) {
+  }
+  @IBAction func medAction(_ sender: Any) {
+  }
+  @IBAction func supAction(_ sender: Any) {
+  }
+  @IBAction func infAction(_ sender: Any) {
+  }
+  
 }
 
